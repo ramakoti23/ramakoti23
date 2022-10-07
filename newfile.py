e82fa3358ac2c3378169bc8e2d9e32c1407ab4ab
Shahid <ramakoti23@gmail.com>
2022-10-07 13:39:40 +0530

    change
    readme commit
    
    Update second.py
    
    first commit in feature
    
    Update newfile.py
    
    stash
    
    Delete second.py
    
    Delete first.py
    
    first rebase
    
    second rebase
    
    Delete second.py
    
    Delete first.py

---
+++ newfile.py
@@ -0,0 +1,5 @@
+this is the new line to start
+this is the second line of new file
+this is the third line
+this is the  fourht  line
+this is the fifth line
--- second.py
+++
@@ -1,2 +0,0 @@
-next line of the secodfile
-this is the first line of second file
